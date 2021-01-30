8ad059fe281ae06f5ae44128f697de4b38eed6f5
gozdetoz <gozdetoz62@gmail.com>
2021-01-31 00:04:55 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,67 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+   public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product>
+            {
+             new Product{ ProductId=1,CategoryId=1,ProductName="Bardak",UnitPrice=15,UnitsInStock=15 }  , 
+             new Product{ ProductId=2,CategoryId=1,ProductName="Kamera",UnitPrice=500,UnitsInStock=3 }  ,
+             new Product{ ProductId=3,CategoryId=2,ProductName="Telefon",UnitPrice=1500,UnitsInStock=2 }  ,
+             new Product{ ProductId=4,CategoryId=2,ProductName="Klavye",UnitPrice=150,UnitsInStock=65 }  ,
+             new Product{ ProductId=5,CategoryId=2,ProductName="Mause",UnitPrice=85,UnitsInStock=1 }  
+            };
+        }
+
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            //Product productoDelete=null;
+            ////linQ olamasaydı dongu yazmak durumunda kalırdık
+            //foreach (var p in _products)
+            //{
+            //    if (product.ProductId == p.ProductId) { productoDelete = p; }
+            //}
+
+            //LINQ -Language Integrated Query
+            //Lambda
+            Product producToDelete = _products.SingleOrDefault(p=>p.ProductId==product.ProductId);
+
+            _products.Remove(producToDelete);
+
+        }
+
+        public List<Product> GetAll()
+        {
+
+            return _products;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            Product producToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            producToUpdate.ProductName = product.ProductName;
+            producToUpdate.CategoryId = product.CategoryId;
+            producToUpdate.UnitPrice = product.UnitPrice;
+            producToUpdate.UnitsInStock = product.UnitsInStock;
+        }
+    }
+}
---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //bir classın defaultu internaldır.Sadece o projede olanlar erişebilir .Diğer katmanlardında erişebilmesi için public yaptık
+    public class Product: IEntity
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+
+    }
+}
